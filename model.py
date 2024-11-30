14a5c2dd3fb2b76b23df0657e6bec7ec9fcfadd9
parallals <frjelvez2021@udec.cl>
2024-11-30 17:34:09 -0300

    optileak

--- model.py
+++ model.py
@@ -1,25 +1,45 @@
+from testParser import parse_file
 import gurobipy as gp
 from gurobipy import Model
 from gurobipy import GRB
 import math
 
+
+
+
+file_path = "Instances/Benchmark_1/coord20-5-1.dat"
+parsed_data = parse_file(file_path)
+
+num_customers = parsed_data["num_customers"]
+num_depots = parsed_data["num_depots"]
+depots = parsed_data["depots"]
+customers = parsed_data["customers"]
+vehicle_capacity = parsed_data["vehicle_capacity"]
+depot_capacities = parsed_data["depot_capacities"]
+customer_demands = parsed_data["customer_demands"]
+depot_opening_costs = parsed_data["depot_opening_costs"]
+route_opening_cost = parsed_data["route_opening_cost"]
+distance_matrix = parsed_data["distance_matrix"]
+
 # Crear un Modelo:
 model = Model("F-MDRP")
 # Definir variables:
 
-x = model.addVars(costs, vtype=GRB.BINARY, name="x") #1 = Usado en circuito, 0 = No
 
-y = model.addVars(depots, vtype=GRB.BINARY,name = "y") #1 = Depot usado en circuito, 0 = No
 
-c = model.addVars(distances, vtype=GRB.BINARY, name= "c") # Costo asociado a cada arco
+x = model.addVars(customers, customers, vtype=GRB.BINARY, name="x")  # Variables de las rutas, si un customer se encuentra con otro, es 1. Si no, 0.
+
+y = model.addVars(num_depots, vtype=GRB.BINARY, name="y")  # Variable de depots de inicio
+
+c = model.addVars(distance_matrix, vtype=GRB.BINARY, name= "c") # Costo asociado a cada arco
 
 # Objective: Minimize total cost
-model.setObjective(sum(c_ij * x_ij for (i, j)),  GRB.MINIMIZE)
+model.setObjective(sum(c[i, j] * x[i, j] for (i, j) in c), GRB.MINIMIZE)
 
 # Restricciones
-# Cada cliente es limitado por un unico depot
-for d in depots:
-    model.addConstr(sum(x[d, j] for j in clients) == y[d], name=f"UsoDepot_{d}")
+# Cada cliente es limitado una única vez
+for j in range(num_customers):
+    model.addConstr(sum(x[i, j] for i in range(num_customers) if i != j) == 1, name=f"VisitOnce_{j}")
 
 # Each client is visited exactly once
 for i in clients:
